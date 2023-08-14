a5d43cb7afbd7bd8cbc578f2e8d87ee359f32bd9
Programmer Sagor <133950257+ProgrammerSagor@users.noreply.github.com>
2023-08-14 18:20:25 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
